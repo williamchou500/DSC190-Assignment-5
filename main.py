133b191949ac22db436308da0c9b2cf994f71eed
williamchou500 <williamchou500@gmail.com>
2026-05-04 20:44:12 -0700

    modified main.py to break ruff rules

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+string-or-bytes-too-long = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+for i in range(10):
+
+twelve = 12
+	unexpected_indent = "hi"
